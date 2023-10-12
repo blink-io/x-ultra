6385aa852c194b863cf59429220d26e136191f2b
heisonyee <heisonyee@126.com>
2023-10-12 15:26:09 +0800

    Add build tag for sql builder

--- sql/builder.go
+++ sql/builder.go
@@ -1,3 +1,5 @@
+//go:build sql_builder
+
 package sql
 
 import (
--- sql/db_test.go
+++ sql/db_test.go
@@ -1,29 +1,9 @@
 package sql
 
 import (
-	"database/sql"
-	"fmt"
 	"testing"
-
-	sq "github.com/Masterminds/squirrel"
-	"github.com/stretchr/testify/require"
 )
 
-func TestSQL_Builder_1(t *testing.T) {
-	select1 := B().
-		Select("name", "age", "level").
-		From("users").
-		Where(sq.And{
-			sq.Eq{"name": "Heison"},
-			sq.Eq{"level": "L1"},
-		})
+func TestSQL_DB_1(t *testing.T) {
 
-	sql1, params1, err := select1.ToSql()
-	require.NoError(t, err)
-
-	sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
-	fmt.Print("SQL1:  ", sql1, "\nParams1:  ", params1, "\n")
-
-	var db *sql.DB
-	db.Exec(sql1, params1)
 }
