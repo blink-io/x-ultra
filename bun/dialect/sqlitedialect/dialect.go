713b2dec31184203e7005c46e66059c00e9334a3
heisonyee <heisonyee@126.com>
2023-10-20 11:15:23 +0800

    Fix bug

--- bun/dialect/sqlitedialect/dialect.go
+++ bun/dialect/sqlitedialect/dialect.go
@@ -22,8 +22,8 @@ func New(ops ...Option) *Dialect {
 // Let the developers make the decision.
 func (d *Dialect) AppendTime(b []byte, tm time.Time) []byte {
 	b = append(b, '\'')
-	if d.opt.local {
-		tm = tm.Local()
+	if d.opt.utc {
+		tm = tm.UTC()
 	}
 	b = tm.AppendFormat(b, "2006-01-02 15:04:05.999999-07:00")
 	b = append(b, '\'')
