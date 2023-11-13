06c3f550e22b3f8bb897672e4933a306a934ddb2
heisonyee <heisonyee@126.com>
2023-11-13 18:05:22 +0800

    Add govalidator codes

--- sql/scany/dbscan/helpers_test.go
+++ sql/scany/dbscan/helpers_test.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -28,14 +29,39 @@ const (
 	`
 )
 
+// RowsAdapter makes pgx.Rows compliant with the dbscan.Rows interface.
+// See dbscan.Rows for details.
+type RowsAdapter struct {
+	pgx.Rows
+}
+
+// NewRowsAdapter returns a new RowsAdapter instance.
+func NewRowsAdapter(rows pgx.Rows) *RowsAdapter {
+	return &RowsAdapter{Rows: rows}
+}
+
+// Columns implements the dbscan.Rows.Columns method.
+func (ra RowsAdapter) Columns() ([]string, error) {
+	columns := make([]string, len(ra.Rows.FieldDescriptions()))
+	for i, fd := range ra.Rows.FieldDescriptions() {
+		columns[i] = fd.Name
+	}
+	return columns, nil
+}
+
+// Close implements the dbscan.Rows.Close method.
+func (ra RowsAdapter) Close() error {
+	ra.Rows.Close()
+	return nil
+}
+
 func makeStrPtr(v string) *string { return &v }
 
 func queryRows(t *testing.T, query string) dbscan.Rows {
 	t.Helper()
-	//pgxRows, err := testDB.Query(ctx, query)
-	//require.NoError(t, err)
-	//rows := sqlscan.NewRowsAdapter(pgxRows)
-	var rows dbscan.Rows
+	pgxRows, err := testDB.Query(ctx, query)
+	require.NoError(t, err)
+	rows := NewRowsAdapter(pgxRows)
 	return rows
 }
 
