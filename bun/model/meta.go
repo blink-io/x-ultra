2e61ef94faf7bfe2a1ef4a81d0cca87e67614044
heisonyee <heisonyee@126.com>
2023-11-21 17:14:39 +0800

    refactor codes

--- bun/model/meta.go
+++ bun/model/meta.go
@@ -1,10 +1,10 @@
 package model
 
 type Meta[T any, C any] struct {
-	PK      string
-	Label   string
-	Table   string
-	Alias   string
-	Type    *T
-	Columns *C
+	PK        string
+	Label     string
+	TableName string
+	Alias     string
+	Type      *T
+	Columns   *C
 }
