e905d817e9c7e0ada5ee1fa12485973a6fb4948c
heisonyee <heisonyee@126.com>
2024-01-25 11:32:39 +0800

    Refactor sql modules

--- postgres/dsn.go
+++ postgres/dsn.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"net"
 	"net/url"
+	"os"
+	"os/user"
+	"path/filepath"
 	"strings"
 )
 
@@ -17,34 +20,35 @@ func isIPOnly(host string) bool {
 type Settings map[string]string
 
 func NewSettings() Settings {
-	return make(Settings)
+	settings := defaultSettings()
+	return settings
 }
 
-func (s Settings) Set(key, val string) {
-	s[key] = val
+func (ss Settings) Set(key, val string) {
+	ss[key] = val
 }
 
-func (s Settings) ToDSN() (string, error) {
+func (ss Settings) ToDSN() (string, error) {
 	return "", nil
 }
 
-func (s Settings) ToURL() (string, error) {
+func (ss Settings) ToURL() (string, error) {
 	return "", nil
 }
 
-func (s Settings) ParseDSN(dsn string) error {
-	if s == nil {
+func (ss Settings) ParseDSN(dsn string) error {
+	if ss == nil {
 		return errors.New("settings is nil")
 	}
-	_, err := doParseDSN(dsn, s)
+	_, err := doParseDSN(dsn, ss)
 	return err
 }
 
-func (s Settings) ParseURL(urlstr string) error {
-	if s == nil {
+func (ss Settings) ParseURL(urlstr string) error {
+	if ss == nil {
 		return errors.New("settings is nil")
 	}
-	_, err := doParseURL(urlstr, s)
+	_, err := doParseURL(urlstr, ss)
 	return err
 }
 
@@ -57,12 +61,12 @@ func ToURL(s Settings) (string, error) {
 }
 
 func ParseURL(urlstr string) (Settings, error) {
-	return doParseURL(urlstr, nil)
+	return doParseURL(urlstr, defaultSettings())
 }
 
 func doParseURL(urlstr string, settings Settings) (Settings, error) {
 	if settings == nil {
-		settings = make(Settings)
+		settings = NewSettings()
 	}
 
 	url, err := url.Parse(urlstr)
@@ -127,12 +131,12 @@ func doParseURL(urlstr string, settings Settings) (Settings, error) {
 }
 
 func ParseDSN(dsn string) (Settings, error) {
-	return doParseDSN(dsn, nil)
+	return doParseDSN(dsn, defaultSettings())
 }
 
 func doParseDSN(dsn string, settings Settings) (Settings, error) {
 	if settings == nil {
-		settings = make(Settings)
+		settings = NewSettings()
 	}
 
 	nameMap := map[string]string{
@@ -205,6 +209,66 @@ func doParseDSN(dsn string, settings Settings) (Settings, error) {
 }
 
 func defaultSettings() Settings {
-	settings := NewSettings()
+	settings := make(Settings)
+
+	settings["host"] = defaultHost()
+	settings["port"] = "5432"
+
+	// Default to the OS user name. Purposely ignoring err getting user name from
+	// OS. The client application will simply have to specify the user in that
+	// case (which they typically will be doing anyway).
+	user, err := user.Current()
+	if err == nil {
+		settings["user"] = user.Username
+		settings["passfile"] = filepath.Join(user.HomeDir, ".pgpass")
+		settings["servicefile"] = filepath.Join(user.HomeDir, ".pg_service.conf")
+		sslcert := filepath.Join(user.HomeDir, ".postgresql", "postgresql.crt")
+		sslkey := filepath.Join(user.HomeDir, ".postgresql", "postgresql.key")
+		if _, err := os.Stat(sslcert); err == nil {
+			if _, err := os.Stat(sslkey); err == nil {
+				// Both the cert and key must be present to use them, or do not use either
+				settings["sslcert"] = sslcert
+				settings["sslkey"] = sslkey
+			}
+		}
+		sslrootcert := filepath.Join(user.HomeDir, ".postgresql", "root.crt")
+		if _, err := os.Stat(sslrootcert); err == nil {
+			settings["sslrootcert"] = sslrootcert
+		}
+	}
+
+	settings["target_session_attrs"] = "any"
+
+	return settings
+}
+
+// defaultHost attempts to mimic libpq's default host. libpq uses the default unix socket location on *nix and localhost
+// on Windows. The default socket location is compiled into libpq. Since pgx does not have access to that default it
+// checks the existence of common locations.
+func defaultHost() string {
+	candidatePaths := []string{
+		"/var/run/postgresql", // Debian
+		"/private/tmp",        // OSX - homebrew
+		"/tmp",                // standard PostgreSQL
+	}
+
+	for _, path := range candidatePaths {
+		if _, err := os.Stat(path); err == nil {
+			return path
+		}
+	}
+
+	return "localhost"
+}
+
+func mergeSettings(settingSets ...Settings) Settings {
+	settings := make(Settings)
+
+	for _, si := range settingSets {
+		for k, v := range si {
+			settings[k] = v
+		}
+	}
+
 	return settings
 }
